f0cae3f5a0982491b9d47d79d92144fe6f428c98
Bea <beata.ficek@gmail.com>
2018-06-26 16:01:54 +0100

    first 2 tests

--- ruby_functions.rb
+++ ruby_functions.rb
@@ -1,3 +1,11 @@
 def return_10
 return 20 / 2
 end
+
+def add(first_number, second_number)
+return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+return first_number - second_number
+end
