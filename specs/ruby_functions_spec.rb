163bb4840642fbdbeb54a9f5506d94be4681f650
Bea <beata.ficek@gmail.com>
2018-06-26 19:27:20 +0100

    no errors

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -42,57 +42,59 @@ class FunctionsTest < MiniTest::Test
     joined_string = join_string( string_1, string_2 )
     assert_equal( "Mary had a little lamb, its fleece was white as snow", joined_string )
   end
+
+  def test_add_string_as_number()
+    add_result = add_string_as_number( "1", "2" )
+    assert_equal( 3, add_result )
+  end
+
+  def test_number_to_full_name_month_1()
+    result = number_to_full_month_name( 1 )
+    assert_equal( "January", result )
+  end
+
+  def test_number_to_full_name_month_3()
+    result = number_to_full_month_name( 3 )
+    assert_equal( "March", result )
+  end
+
+  def test_number_to_full_name__month_9()
+    result = number_to_full_month_name( 9 )
+    assert_equal( "September", result )
+  end
+
+  def test_substring_month_1()
+    first_month_string = number_to_short_month_name( 1 )
+    assert_equal( "Jan", first_month_string )
+  end
+
+  def test_substring__month_4()
+    fourth_month_string = number_to_short_month_name( 4 )
+    assert_equal( "Apr", fourth_month_string )
+  end
   #
-  # def test_add_string_as_number()
-  #   add_result = add_string_as_number( "1", "2" )
-  #   assert_equal( 3, add_result )
-  # end
-  #
-  # def test_number_to_full_name__month_1()
-  #   result = number_to_full_month_name( 1 )
-  #   assert_equal( "January", result )
-  # end
-  #
-  # def test_number_to_full_name__month_3()
-  #   result = number_to_full_month_name( 3 )
-  #   assert_equal( "March", result )
-  # end
-  #
-  # def test_number_to_full_name__month_9()
-  #   result = number_to_full_month_name( 9 )
-  #   assert_equal( "September", result )
-  # end
-  #
-  # def test_substring__month_1()
-  #   first_month_string = number_to_short_month_name( 1 )
-  #   assert_equal( "Jan", first_month_string )
-  # end
-  #
-  # def test_substring__month_4()
-  #   fourth_month_string = number_to_short_month_name( 4 )
-  #   assert_equal( "Apr", fourth_month_string )
-  # end
-  #
-  # def test_substring__month_10()
-  #   tenth_month_string = number_to_short_month_name( 10 )
-  #   assert_equal( "Oct", tenth_month_string )
-  # end
-  #
+  def test_substring__month_10()
+    tenth_month_string = number_to_short_month_name( 10 )
+    assert_equal( "Oct", tenth_month_string )
+  end
+
   # #Further
   #
-  # #Given the length of a side of a cube calculate the volume
-  # def test_volume_of_cube()
-  #   #add test code here
-  # end
-  #
-  # #Given the radius of a sphere calculate the volume
-  # def test_volume_of_sphere()
-  #   #add test code here
-  # end
-  #
-  # #Given a value in farenheit, convert this into celsius.
-  # def test_fahrenheit_to_celsius()
-  #   #add test code here
-  # end
+  #Given the length of a side of a cube calculate the volume
+  def volume_of_cube()
+    length_of_side = volume_of_cube(3)
+  assert_equal(27, length_of_side)
+  end
 
+  # #Given the radius of a sphere calculate the volume
+  def volume_of_sphere()
+    length_of_radius = volume_of_sphere(2)
+  assert_equal(34, length_of_radius)
+  end
+
+  # #Given a value in farenheit, convert this into celsius.
+  def fahrenheit_to_celsius()
+    celsius = fahrenheit_to_celsius(32)
+    assert_equal(0, celsius)
+  end
 end
